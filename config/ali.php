96032352a858009b9ae0e3ee510eea7b106d3cde
xiaoxiao0301 <jixiao35@qq.com>
2020-06-13 15:58:22 +0800

    delete aliyun sms key

--- config/ali.php
+++ config/ali.php
@@ -1,10 +1,10 @@
 <?php
 // 阿里云短信服务相关配置
 return [
-    'accessKeyId' => 'LTAI4FnG6xyu8UUAvWgFyFoH',
+    'accessKeyId' => '',
     'accessSecret' => '',
     // 短信签名
     'SignNameJson' =>  'Lbbs社区',
     // 模板code
-    'TemplateCode' =>  'SMS_179611210'
+    'TemplateCode' =>  ''
 ];
